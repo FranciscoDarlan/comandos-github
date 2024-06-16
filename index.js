f9e618a5ffd52ab89a28909553646238016f3908
Francisco Darlan <118319737+FranciscoDarlan@users.noreply.github.com>
2024-06-15 22:13:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // vamos iniciar os comandos git
+
+por padrão o windons tem alguns terminais proprios no windown é o CMD e powerShell..
+  gitbash - vamos usar esse, ele é instalado quando instalamos o git
+command prompt
+para não precisar ficar selecionando o git bash toda hora vamos dar o comando 
+ctrl+shift+p = vai abrir um seletor e vamos colocar terminal:select default profile!
+e temos que trocar o terminal padrão para git bash!
+
+vamos configurar o nosso email do github no terminal... para logar localmente
+  git config --global user.email emailcadastrado@gmail.com    enter se 
+
+git config --global user.name nomeusuariogithub
+
+git config  --list  = para verificar o nome e email se esta certo no final os ultimos infomes
+
+precisa configura o vscode no simbolo de boneco sign in with github
+ele vai direcionar para o site do git para conectar o vscode
+com email e senha ...
+
+  e vai logar!
